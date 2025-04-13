25033f842d4704c402b372428a40c8c5daa88802
Jose Andres Macas Velez <83767176+andresmacas@users.noreply.github.com>
2025-04-12 23:49:26 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,10 @@ const nextConfig: NextConfig = {
   i18n: {
     locales: ['es', 'en'],
     defaultLocale: 'es'
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
